e82162e1511202278c9bc5a55c46ba5a73bdb76b
Q4mn44XH <Q4mn44XH@users.noreply.github.com>
2013-10-22 22:03:52 -0700

    Replaced tabs with spaces.

--- test_01/test_01.js
+++ test_01/test_01.js
@@ -5,30 +5,30 @@ $(document).ready(function() {
     var fail_count = 0 ;
     var test_interval_ms = 1000 ;
     var get_ajax_obj = function() {
-	return {
-	    url: "http://localhost:8090/test_request.html", 
-	    timeout: 3000, 
-	    dataType: 'jsonp',
-	    success: function(data) {
-		pass_count++ ;
-		$('#test_result').text(data['result'] ? "True" : "False") ;
-		$('#pass_count').text(pass_count) ;
-		run_next_test() ;
-	    }, 
-	    error: function() {
-		fail_count++ ;
-		$('#fail_count').text(fail_count) ;
-		run_next_test() ;
-	    }
-	}
+        return {
+            url: "http://localhost:8090/test_request.html", 
+            timeout: 3000, 
+            dataType: 'jsonp',
+            success: function(data) {
+                pass_count++ ;
+                $('#test_result').text(data['result'] ? "True" : "False") ;
+                $('#pass_count').text(pass_count) ;
+                run_next_test() ;
+            }, 
+            error: function() {
+                fail_count++ ;
+                $('#fail_count').text(fail_count) ;
+                run_next_test() ;
+            }
+        }
     } ;
 
     var run_next_test = function() { 
-	setTimeout( function() {
-	    test_count++ ;
-	    $('#test_count').text(test_count) ;
-	    $.ajax(get_ajax_obj()); 
-	}, test_interval_ms ) ;
+        setTimeout( function() {
+            test_count++ ;
+            $('#test_count').text(test_count) ;
+            $.ajax(get_ajax_obj()); 
+        }, test_interval_ms ) ;
     } ;
 
     run_next_test() ;
